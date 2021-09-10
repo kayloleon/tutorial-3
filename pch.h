f6d460d90e3947cc60a2701e073dcb5ba2b9f0c0
kayloleon <69911814+kayloleon@users.noreply.github.com>
2021-09-09 21:50:10 -0400

    Add files via upload

---
+++ pch.h
@@ -0,0 +1,9 @@
+﻿// pch.h
+#pragma once
+#include <winrt/Windows.Foundation.Collections.h>
+#include <winrt/Windows.Web.Syndication.h>
+#include <iostream>
+
+#include <winrt/base.h>
+
+#pragma comment(lib, "windowsapp")
